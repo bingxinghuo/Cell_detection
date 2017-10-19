c824553d07a1973a42e2f1c844b6bb01f7a9b7b9
Bingxing Huo <bingxing.huo@gmail.com>
2017-10-19 13:16:05 -0400

    added cross registration python files

--- scratch code/ML/SVM/svmcellwrapper_short.m
+++ scratch code/ML/SVM/svmcellwrapper_short.m
@@ -1,10 +1,10 @@
-function []=svmcellwrapper_bnb()
+function []=svmcellwrapper_short()
 myCluster = parcluster('local'); % cores on compute node to be "local"
 addpath(genpath('~/'))
 brainids={'m920';'m919'};
-poolobj=parpool(myCluster, 12);
-for d=2:2
-    
+poolobj=parpool(myCluster, 4);
+for d=1:1
+
     brainid=brainids{d};
     % read JP2 files from mitraweb2
     cd(['/nfs/mitraweb2/mnt/disk125/main/marmosetRIKEN/NZ/',brainid,'/',brainid,'F/JP2-REG'])
@@ -13,20 +13,20 @@ for d=2:2
     % read file list
     filelist=jp2lsread;
     Nf=length(filelist);
-    failmsg=cell(Nf,1); % allocate a vector to catch failures
-    parfor f=1:Nf
+    failcatch=zeros(Nf,1); % allocate a vector to catch failures
+    parfor f=1:2
         try
             fileid=filelist{f};
             fileout=['~/',brainid,'/cells/',fileid(1:end-4),'_cells.jp2'];
-            if ~exist(fileout,'file') % avoid re-processing detections
+            if exist(fileout,'file')~=2 % avoid re-processing detections
                 fluoroimg=imread(fileid,'jp2');
                 svmcellblock(fluoroimg,2,svmmodel,fileout)
             end
         catch ME
             f
-            failmsg{f}=ME;
+            failcatch(f)=1;
+	    rethrow(ME)
         end
     end
-    save(['~/',brainid,'/failimgs'],'failmsg')
 end
 delete(poolobj)
