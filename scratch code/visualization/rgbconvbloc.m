d56ef70c5453e0430269237ffb6b95fe2e19cc5c
Bingxing Huo <bingxing.huo@gmail.com>
2017-10-19 16:55:19 -0400

    run conversion on bnb

--- scratch code/visualization/rgbconvbloc.m
+++ scratch code/visualization/rgbconvbloc.m
@@ -11,13 +11,29 @@
 % BG=cat(3,channelpad,channelpad,uint16(block_struct.data*(2^8-1)));
 % end
 %% 8 bit
+% This part of the code aims at converting either binary or 16-bit black/blue JP2
+% into 8-bit black/white JP2
 function rgbconvbloc(filein,fileout)
-myFun = @(block_struct) blockgradient(block_struct);
+bitinfo=imfinfo(filein);
+bitinfo=bitinfo.BitDepth;
+if bitinfo==1
+    myFun = @(block_struct) blockbin2rgb(block_struct);
+elseif bitinfo==48
+    myFun = @(block_struct) block16to8bit(block_struct);
+else
+    disp([filein,' format is incorrect!'])
+end
 block_size=[512,512];
 blockproc(filein,block_size,myFun,'Destination',fileout);
 end
 
-function BG=blockgradient(block_struct)
+function cellrgbmask=blockbin2rgb(block_struct)
 cellmask=uint8(block_struct.data*(2^8-1));
-BG=cat(3,cellmask,cellmask,cellmask);
+cellrgbmask=cat(3,cellmask,cellmask,cellmask);
+end
+
+
+function cellrgbmask=block16to8bit(block_struct)
+cellmask=uint8(block_struct.data(:,:,3));
+cellrgbmask=cat(3,cellmask,cellmask,cellmask);
 end
--- scratch code/visualization/rgbconvwrapper_bnb.m
+++ scratch code/visualization/rgbconvwrapper_bnb.m
@@ -2,13 +2,13 @@ brainids={'m920';'m919'};
 for d=1:2
     brainid=brainids{d};
     %     cd(['~/marmosetRIKEN/NZ/',brainid,'/',brainid,'F/JP2-REG/cellmasks/binaryimg'])
-    cd(['~/marmosetRIKEN/NZ/',brainid,'/',brainid,'F/JP2-REG/cellmasks/16bitRGBnew/'])
+    cd(['/nfs/mitraweb2/mnt/disk125/main/marmosetRIKEN/NZ/',brainid,'/',brainid,'F/JP2-REG/cellmasks/16bitRGBnew/'])
     filelist=jp2lsread;
     Nf=length(filelist);
     parfor f=1:Nf
         filein=filelist{f};
         %         fileout=[fileid(1:end-4),'16rgb.jp2'];
-        fileout=['../8bitRGBnew/',filein];
+        fileout=['~/marmosetdata/',brainid,'/8bitRGBnew/',filein];
         if exist(fileout,'file')~=2 % avoid re-processing detections
             rgbconvbloc(filein,fileout)
         end
