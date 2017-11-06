326c6179a5782d9b1aa8c217cc200c908b64fd22
Bingxing Huo <bingxing.huo@gmail.com>
2017-11-06 17:34:00 -0500

    moved rmbg.m to DAP; cleaned up svm function

--- scratch_code/ML/SVM/svmcellblock.m
+++ scratch_code/ML/SVM/svmcellblock.m
@@ -1,9 +1,16 @@
+%% svmcellblock.m
+% This function utilizes Matlab's built-in blocproc function for processing
+% large images to read blocks of JP2 images, perform cell detection, and
+% save directly into a new JP2 image containing the cell masks. 
+
 function svmcellblock(fluoroimg,n,svmmodel,fileout)
+% function newimg=svmcellblock(fluoroimg,n,svmmodel)
 myfun = @(block_struct) svmcell(block_struct,n,svmmodel);
 block_size=[512,512];
 border_size=[n*5,n*5];
 blockproc(fluoroimg,block_size,myfun,'BorderSize',border_size,...
     'Destination',fileout,'UseParallel',true,'DisplayWaitbar',false);
+% newimg=blockproc(fluoroimg,block_size,myfun,'BorderSize',border_size);
 end
 
 function cellmask=svmcell(block_struct,n,svmmodel)
@@ -13,6 +20,7 @@ if sum(sum(block_struct.data(:,:,3)>50))>100
     nhoodimg=nhoodfeature(block_struct.data,n);
     [rows,cols,features]=size(nhoodimg);
     nhoodfeatures=reshape(nhoodimg,rows*cols,features);
+    % use the previously trained model to predict cell masks
     cellscore=predict(svmmodel,nhoodfeatures);
     cellmask=reshape(cellscore,rows,cols);
     cellmask=uint16(cellmask*2^16);
