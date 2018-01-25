d7ae71e43a36fc7374e428f190a06d64a7af5002
Bingxing Huo <bingxing.huo@gmail.com>
2018-01-25 14:27:05 -0500

    debugged jp22pngtile.m

--- scratch_code/visualization/imgconversions/jp22pngtile.m
+++ scratch_code/visualization/imgconversions/jp22pngtile.m
@@ -1,3 +1,7 @@
+%% jp22pngtile.m
+% This script was prepared for IIT manual annotation
+% This script combines the first pass cell detection result with the
+% original registered image, convert them into 512x512 png tiled images.
 function jp22pngtile(fluoroimg1,FBclear,fileind,fileid)
 global blk
 [rows,cols,~]=size(fluoroimg1);
@@ -13,7 +17,7 @@ for h=1:H
         blkcellind=(FBcells(:,2)>=(h-1)*blk+1).*(FBcells(:,2)<=h*blk).*...
             (FBcells(:,1)>=(w-1)*blk+1).*(FBcells(:,1)<=w*blk);
         blkcell=FBcells(find(blkcellind),:);
-        blkcellwhite=celloverlay(blkcell,h,w);
+        blkcellwhite=celloverlay(blkcell,h,w,blk,blk);
         blkimgcombine=blkcellwhite+blkimg;
         pngtilefile=[fileid(1:end-4),'_t',num2str(k),'.png'];
         imwrite(blkimgcombine,pngtilefile,'png');
@@ -23,7 +27,7 @@ for h=1:H
     blkcellind=(FBcells(:,2)>=(h-1)*blk+1).*(FBcells(:,2)<=h*blk).*...
         (FBcells(:,1)>=W*blk+1);
     blkcell=FBcells(find(blkcellind),:);
-    blkcellwhite=celloverlay(blkcell,h,W+1);
+    blkcellwhite=celloverlay(blkcell,h,W+1,blk,cols-W*blk);
     blkimgcombine=blkcellwhite+blkimg;
     pngtilefile=[fileid(1:end-4),'_t',num2str(k),'.png'];
     imwrite(blkimgcombine,pngtilefile,'png');
@@ -34,7 +38,7 @@ for w=1:W
     blkcellind=(FBcells(:,2)>=H*blk+1).*...
         (FBcells(:,1)>=(w-1)*blk+1).*(FBcells(:,1)<=w*blk);
     blkcell=FBcells(find(blkcellind),:);
-    blkcellwhite=celloverlay(blkcell,H+1,w);
+    blkcellwhite=celloverlay(blkcell,H+1,w,rows-H*blk,blk);
     blkimgcombine=blkcellwhite+blkimg;
     pngtilefile=[fileid(1:end-4),'_t',num2str(k),'.png'];
     imwrite(blkimgcombine,pngtilefile,'png');
@@ -43,17 +47,17 @@ end
 blkimg=fluoroimg1(H*blk+1:end,W*blk+1:end,:);
 blkcellind=(FBcells(:,2)>=H*blk+1).*(FBcells(:,1)>=W*blk+1);
 blkcell=FBcells(find(blkcellind),:);
-blkcellwhite=celloverlay(blkcell,H+1,W+1);
+blkcellwhite=celloverlay(blkcell,H+1,W+1,rows-H*blk,cols-W*blk);
 blkimgcombine=blkcellwhite+blkimg;
 pngtilefile=[fileid(1:end-4),'_t',num2str(k),'.png'];
 imwrite(blkimgcombine,pngtilefile,'png');
 end
 %%%%%%%%%
-function blkcellwhite=celloverlay(blkcell,h,w)
+function blkcellwhite=celloverlay(blkcell,h,w,imH,imW)
 global blk
 se=strel('diamond',3);
 blkcell=blkcell-ones(size(blkcell,1),1)*[(w-1)*blk,(h-1)*blk];
-blkcellwhite=logical(zeros(blk,blk));
+blkcellwhite=logical(zeros(imH,imW));
 for i=1:size(blkcell,1)
     blkcellwhite(blkcell(i,2),blkcell(i,1))=true;
 end
