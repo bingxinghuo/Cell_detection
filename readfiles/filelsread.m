7a8a0bb51aff5bfb98b3ae554fc342bc7eb3df75
Bingxing Huo <bhuo@mitraimacpro-bh.cshl.edu>
2019-08-29 10:16:55 -0400

    cleaned up mask generation directory

--- readfiles/filelsread.m
+++ readfiles/filelsread.m
@@ -4,13 +4,16 @@
 %   - none. Make sure the working directory has all the JP2 files
 % Output:
 %   - filelist: a N-by-1 cell array containing the file name in each cell
-function filelist=filelsread(filespec)
+function filelist=filelsread(filespec,savedir)
 % If there is no list of file names, create one using shell script
-if ~exist([pwd,'/','filenames.txt'],'file')
-    system(['ls -h ', filespec, ' | sort -t"_" -k3 > filenames.txt']);
+if nargin==1
+    savedir='./';
+end
+if ~exist([savedir,'/filenames.txt'],'file')
+    system(['ls -h ', filespec, ' | sort -t"_" -k3 > ',savedir,'/filenames.txt']);
 end
 % read the file list from the text file
-fid=fopen('filenames.txt');
+fid=fopen([savedir,'/filenames.txt']);
 filelist=textscan(fid,'%q');
 fclose(fid);
 filelist=filelist{1};
