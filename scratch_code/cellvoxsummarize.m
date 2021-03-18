7612a22e45472bef720b66d248fd7bad3c6a8f1f
Bingxing Huo <bhuo@mitraimacpro-bh.cshl.edu>
2021-03-18 11:33:50 -0400

    updated cell summarization

--- scratch_code/cellvoxsummarize.m
+++ scratch_code/cellvoxsummarize.m
@@ -1,5 +1,5 @@
-%% 1 summarize into desired voxel size, isotropic
-simgscale=64;
+%% 1 CELL MASK: summarize into desired voxel size, isotropic
+simgscale=64; % 64X downsample
 disp(['Summarizing the ',detecttype,' masks into ',num2str(datainfo.voxelsize(1)),'µm volume... '])
 if isempty(imgdir)
     warning(['Dataset not transformed yet. Skip ',brainID])
@@ -25,28 +25,30 @@ else
 end
 outputfile=[outputdir,'/',brainID,'_',detecttype,'_',num2str(datainfo.voxelsize(1)),'.mat'];
 %% 2 load segmentation
-% e.g. atlas-seg_to_M826-F69--_1_0137.vtk
-[x,y,z,segimg,title,names,spacing,origin] = read_vtk_image('atlas-seg_to_M826-F69--_1_0137.vtk')
-%% 3 cell centroids for this section
-fbcelltf=FBnissl{fileinds_nissl(n)};
-fbcelltfdown=round(fbcelltf/M);
-if ~isempty(fbcelltfdown)
-    % remove out of boundary coordinates
-    [ind1,~]=find(fbcelltfdown<=0);
-    ind2=find(fbcelltfdown(:,1)>imgwidth);
-    ind3=find(fbcelltfdown(:,2)>imgheight);
-    fbcelltfdown([ind1;ind2;ind3],:)=[];
-    % read out the annotation indices
-    annoind=zeros(size(fbcelltfdown,1),1);
-    for i=1:size(fbcelltfdown,1)
-        annoind(i)=regmask(fbcelltfdown(i,2),fbcelltfdown(i,1))/15;
-    end
-    % statistics
-    annoids=unique(annoind);
-    if length(annoids)>1
-        annoids=nonzeros(annoids); % remove 0
-        for d=1:length(annoids)
-            regcount(n,annoids(d))=sum(annoind==annoids(d)); % count cells within individual region
+[r,c,S]=size(neurondensity);
+% S is the number of sections
+for i=1:S
+    filename
+    % e.g. atlas-seg_to_M826-F69--_1_0137.vtk
+    [x,y,z,regmask,title,names,spacing,origin] = read_vtk_image(['atlas-seg_to_',filename,'.vtk']);
+    %% 3 cell centroids for this section
+    fbcelltf=FBnissl{fileinds_nissl(i)}; % each Matlab cell contains the coordinates of all cells in high resolution image
+    fbcelltfdown=round(fbcelltf/simgscale); % scale down coordinates 
+    if ~isempty(fbcelltfdown)
+        % remove out of boundary coordinates
+        [ind1,~]=find(fbcelltfdown<=0);
+        ind2=find(fbcelltfdown(:,1)>imgwidth);
+        ind3=find(fbcelltfdown(:,2)>imgheight);
+        fbcelltfdown([ind1;ind2;ind3],:)=[];
+        % read out the annotation indices
+        annoid=unique(regmask); % all region IDs within the section
+        annoid=nonzeros(annoid); % remove 0
+                % statistics
+        if length(annoids)>1
+            for d=1:length(annoids)
+                regcount{i}(d,1)=annoids(d); % brain region ID
+                regcount{i}(annoids(d),2)=sum(annoid==annoids(d)); % count cells within individual region
+            end
         end
     end
 end
