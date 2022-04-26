260f9a8d7a41d40e32723e7948c6904e0bec9ef1
Bingxing Huo <bhuo@mitraimacpro-bh.cshl.edu>
2022-04-26 12:38:11 -0400

    updated background setting code

--- scratch_code/histograms/bgstandard.m
+++ scratch_code/histograms/bgstandard.m
@@ -1,4 +1,4 @@
-function [brainimg0,bgimgmed0,jp2file0]=bgstandard(filelist,tifdir,maskdir,savedir)
+function [brainimg0,bgimgmed0,jp2file0]=bgstandard(filelist,tifdir,maskdir,M,savedir)
 secid0=input('Please set a section as the standard: ','s');
 [f0,jp2file0]=jp2ind(filelist,secid0);
 [~,filename,~]=fileparts(jp2file0);
@@ -7,11 +7,14 @@ maskfile0=[maskdir,'/',filename,'.tif'];
 if ~exist(maskfile0,'file')
     maskfile0=[maskdir,'/imgmaskdata_',num2str(f0)];
     if ~exist(maskfile0,'file')
-        disp('Please generate mask images for jp2 files first!')
-        disp('Hint: Run following code locally: brainmaskbatch.m')
+        maskfile0=[maskfile0,'.tif'];
+        if ~exist(maskfile0,'file')
+            disp('Please generate mask images for jp2 files first!')
+            disp('Hint: Run following code locally: brainmaskbatch.m')
+        end
     end
 end
-[brainimg0,bgimgmed0]=bgmean3(tiffile0,maskfile0);
+[brainimg0,bgimgmed0]=bgmean3(tiffile0,maskfile0,M);
 if nargin>3
     save([savedir,'/background_standard.mat'],'bgimgmed0','brainimg0','jp2file0')
 end
